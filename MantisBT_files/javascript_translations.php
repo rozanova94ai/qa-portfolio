05d1d27a372e2062ff67d70cd52582699280b9bc
rozanova94ai <101975979+rozanova94ai@users.noreply.github.com>
2022-03-20 14:04:31 +0200

    Initial Commit
    Added materials from QA-testlab courses

---
+++ MantisBT_files/javascript_translations.php
@@ -0,0 +1,7 @@
+var translations = new Array();
+translations['time_tracking_stopwatch_start'] = 'Запустить';
+translations['time_tracking_stopwatch_stop'] = 'Остановить';
+translations['loading'] = 'Загрузка...';
+translations['form_error_required'] = 'Это поле обязательно для заполнения';
+translations['form_error_valid_email'] = 'Пожалуйста, введите действительный адрес электронной почты.';
+translations['form_error_maxlength'] = 'Введите не более {0} символов';
---
+++ MantisBT_files/status_colors.css
@@ -0,0 +1,5 @@
+.status-20-color { color: #e3b7eb; background-color: #e3b7eb; }
+.status-25-color { color: #ff9800; background-color: #ff9800; }
+.status-50-color { color: #c2dfff; background-color: #c2dfff; }
+.status-90-color { color: #c9ccc4; background-color: #c9ccc4; }
+.status-legend-width { width: 13%; }
